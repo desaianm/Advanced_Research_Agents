190ed4905c2da69285468075a4bb7d56a9685020
desainanm <desaianm@sheridancollege.ca>
2024-04-04 23:46:07 -0400

    changes

---
+++ agents.py
@@ -0,0 +1,41 @@
+import os
+from crewai import Agent, Task, Crew, Process
+from crewai_tools import SerperDevTool,WebsiteSearchTool, ScrapeWebsiteTool
+
+os.environ['OPENAI_MODEL_NAME'] = 'gpt-4-0125-preview'
+search_tool = SerperDevTool()
+web_search = WebsiteSearchTool()
+web_scrape = ScrapeWebsiteTool()
+
+class Agents():
+# Define your agents with roles and goals
+    def research_analyst(self): 
+        return Agent(
+        role='Research Analyst',
+        goal='find fascinating insights and keep an eye for details. draft a report on the company.',
+        backstory=""" You have been world renowed for your research on companies.""",
+        verbose=True,
+        allow_delegation=True,
+        tools=[search_tool,web_search,web_scrape],
+        max_iter=5
+        
+        )
+    
+    def web_researcher(self):
+        return Agent(
+        role='Web Researcher',
+        goal='Find information on the internet about AI Companies',
+        backstory="""You have been expert at finding information about Companies on the internet.""",
+        verbose=True,
+        allow_delegation=False,
+        tools=[search_tool,web_scrape]
+        )
+    
+    def senior_research_manager(self):
+        return Agent(
+        role='Senior Research Manager',
+        goal='Monitor and Direct the Research Analyst as well as write final draft on research',
+        backstory="""You have been expert at managing people. You have great eye to detail and can write well.""",
+        verbose=True,
+        allow_delegation=True
+        )
---
+++ tasks.py
@@ -0,0 +1,32 @@
+from crewai import Agent, Task, Crew, Process
+
+class Tasks:
+
+    def info_gather(self,agent,text):
+        return Task(
+    description=f""" Company Name: {text} 
+    You will find information about the company on the internet and look for information on Company's Team, Products and Services, Culture, Benefits of Working there, Financials, Recent News and Developments.
+    Keep Searching until you have enough information to write a detailed report on the company. Divide the search into subtasks and keep track of the progress.
+    """,
+    expected_output="Text file with report of the company.",
+    agent=agent
+    )
+
+    def research(self,agent,text):
+        return Task(
+    description=f""" Company Name: {text}
+    You will be given a draft report about company . Check find missing infomration and add more details to the report.
+    you have to provide a in depth report on the above company.
+    Main Headers should be: Company's Team, Products and Services, Culture, Benefits of Working there, Financials, Recent News and Developments.
+    """,
+    expected_output="Text file with detailed analysis of the company.",
+    agent=agent
+    )
+
+    def write(self,agent):
+        return Task(
+        description="""Using the report given by the Research Analyst, write a research paper on the company.
+        Main Headers should be: Company's Team, Products and Services, Culture, Benefits of Working there, Financials, Recent News and Developments.""",
+        expected_output="Full blog post of at least 8 paragraphs with above mentioned headers.",
+        agent=agent
+        )
